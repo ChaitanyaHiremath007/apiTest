5b9bd1760a84a7c28f0a3e58fbde70ec59f9d4dd
ChaitanyaHiremath007 <130186354+ChaitanyaHiremath007@users.noreply.github.com>
2023-04-09 00:39:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("../src/db/conn")
 const EmpData = require("../src/models/emp")
-const port = process.env.PORT || 3000;
+
 
 app.use(express.json())
 
